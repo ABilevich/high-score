70b20c01b1f7d1f6c10b88444de27aef6b395310
AndyBilevich <abilevich@itba.edu.ar>
2021-01-13 15:07:45 -0500

    initial push

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>High Scores:</h1>
+    <input type="text" v-model="newScore" @keyup.enter="addScore">
+    <ul>
+      <li v-for="score of scores" :key="score.id">{{ score.value }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import axios from "axios";
+
+const baseUrl = 'http://localhost:3000/scores';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      scores: [],
+      newScore: "",
+    } 
+  },
+  async created(){
+    try{
+      this.getHighScores()
+    }catch(e){
+      console.log(e);
+    }
+  },
+  methods:{
+    async addScore(){
+      const resp = await axios.post(baseUrl, { value: Number(this.newScore)});
+      this.scores = [...this.scores, resp.data];
+      this.newScore = ""
+      await this.getHighScores();
+    },
+    async getHighScores(){
+      const resp = await axios.get(baseUrl);
+      this.scores = resp.data
+    }
   }
 }
 </script>
@@ -25,4 +53,14 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+li {
+  list-style: none;
+}
+
+ul {
+  margin: 5px;
+  padding: 0px;
+}
+
 </style>
