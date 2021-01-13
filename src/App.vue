20df0673ac3f914a70fc4b92a9df4289f2d3dc08
AndyBilevich <abilevich@itba.edu.ar>
2021-01-13 15:44:07 -0500

    added comments and restructured

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,37 @@
 <template>
   <div id="app">
     <h1>High Scores:</h1>
-    <input type="text" v-model="newScore" @keyup.enter="addScore">
-    <ul>
-      <li v-for="score of scores" :key="score.id">{{ score.value }}</li>
-    </ul>
+    
+    <h3 id="error" v-if="this.err">{{this.errMsg}}</h3>
+
+    <label for="user">User:</label>
+    <input type="text" id="user" v-model="newUserName">
+
+    <br>
+
+    <label for="score">Score:</label>
+    <input type="text" id="score" v-model="newScore">
+
+    <br>
+
+    <button @click="addScore">Submit</button>
+
+    <br><br>
+
+    <div id="scores">
+      <table>
+        <tr>
+          <th>User</th>
+          <th>Score</th>
+        </tr>
+        <tr v-for="score of scores" :key="score.user">
+          <td>{{ score.user }}</td>
+          <td>{{ score.score }}</td>
+        </tr>
+      </table>
+    </div>
+    
+
   </div>
 </template>
 
@@ -12,6 +39,8 @@
 
 import axios from "axios";
 
+//Esto cambialo por la url de tu api, si son distintas para el get y el post hace una var nueva
+//ahora esta usando una "api" simulada con json database que esta en el archivo db.jason, lo podes ignorar.
 const baseUrl = 'http://localhost:3000/scores';
 
 export default {
@@ -20,6 +49,9 @@ export default {
     return {
       scores: [],
       newScore: "",
+      newUserName: "",
+      err: false,
+      errMsg: "empty"
     } 
   },
   async created(){
@@ -30,15 +62,24 @@ export default {
     }
   },
   methods:{
-    async addScore(){
-      const resp = await axios.post(baseUrl, { value: Number(this.newScore)});
-      this.scores = [...this.scores, resp.data];
-      this.newScore = ""
-      await this.getHighScores();
-    },
     async getHighScores(){
+      //aca se pide los scores y se los agrega al array, la idea es que pidas el top 10 aca
       const resp = await axios.get(baseUrl);
       this.scores = resp.data
+    },
+    async addScore(){
+      if(this.newScore == "" || this.newUserName == ""){
+        this.err = true;
+        this.errMsg = "must fill both inputs";
+      }else{
+        this.err = false;
+        //aca se agrega un score cuando apretas enter
+        const resp = await axios.post(baseUrl, { user: this.newUserName, score: Number(this.newScore)});
+        this.scores = [...this.scores, resp.data];
+        this.newScore = "";
+        this.newUserName = "";
+        await this.getHighScores();
+      }
     }
   }
 }
@@ -61,6 +102,35 @@ li {
 ul {
   margin: 5px;
   padding: 0px;
+}
+
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+
+#scores {
+  width: 50%;
+  margin-left: 25%;
+}
+
+#error {
+  color: red;
+}
+
+input {
+  margin: 5px;
 }
 
 </style>
